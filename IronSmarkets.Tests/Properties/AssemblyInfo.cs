c804e3b98e2a682e08c549436d3184d2a18d2c98
Hunter Morris <hunter.morris@smarkets.com>
2011-10-31 11:27:43 +0000

    Add basic unit test project

---
+++ IronSmarkets.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("IronSmarkets.Tests")]
+[assembly: AssemblyDescription(".NET Client tests for the Smarkets exchange")]
--- IronSmarkets/Properties/AssemblyInfo.cs
+++ IronSmarkets/Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("IronSmarkets")]
 [assembly: AssemblyDescription(".NET Client for the Smarkets exchange")]
+
+// For unit tests
+[assembly: InternalsVisibleTo("IronSmarkets.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000110000000d70ffcd5112ea1992cc75a0c9bb57bebfbcc402aa8bb6957103bf1d3b5c19f1cc98178f72d3a3edb860b1c7e9861fca504e56fbe0c9cd4cf2d9f678541bd19f265044f073d6190e193ba52d93fef2d730808cd5196809dc1ce695a93642afeb00f1c59371c51c15f36dc2a0dfb6f2cd20c4f5f158c06afd2ef21af733d1d7dc")]

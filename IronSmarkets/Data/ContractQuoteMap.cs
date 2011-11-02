03b507d0afce1ae470cea7dde494a27fed506859
Hunter Morris <hunter.morris@smarkets.com>
2011-11-02 11:03:45 +0000

    Add MarketQuotes data types

---
+++ IronSmarkets/Data/ContractQuoteMap.cs
@@ -0,0 +1,137 @@
+// Copyright (c) 2011 Smarkets Limited
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use, copy,
+// modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IronSmarkets.Data
+{
+    public interface IContractQuotesMap : IDictionary<Uuid, ContractQuotes>
+    {
+    }
+
+    internal class ContractQuotesMap : IContractQuotesMap
+    {
+        private readonly IDictionary<Uuid, ContractQuotes> _contractQuotes;
+
+        private ContractQuotesMap(IDictionary<Uuid, ContractQuotes> contractQuotes)
+        {
+            _contractQuotes = contractQuotes;
+        }
+
+        public static IContractQuotesMap FromSeto(
+            IEnumerable<Proto.Seto.ContractQuotes> setoContractQuotes)
+        {
+            return new ContractQuotesMap(
+                setoContractQuotes.Aggregate(
+                    new Dictionary<Uuid, ContractQuotes>(),
+                    (dict, contractQuotes) => {
+                        var cq = ContractQuotes.FromSeto(contractQuotes);
+                        dict[cq.Uuid] = cq;
+                        return dict;
+                    }));
+        }
+
+        public IEnumerator<KeyValuePair<Uuid, ContractQuotes>> GetEnumerator()
+        {
+            return _contractQuotes.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(KeyValuePair<Uuid, ContractQuotes> item)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotSupportedException();
+        }
+
+        public bool Contains(KeyValuePair<Uuid, ContractQuotes> item)
+        {
+            return _contractQuotes.Contains(item);
+        }
+
+        public void CopyTo(KeyValuePair<Uuid, ContractQuotes>[] array, int arrayIndex)
+        {
+            _contractQuotes.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(KeyValuePair<Uuid, ContractQuotes> item)
+        {
+            throw new NotSupportedException();
+        }
+
+        public int Count
+        {
+            get { return _contractQuotes.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return true; }
+        }
+
+        public bool ContainsKey(Uuid key)
+        {
+            return _contractQuotes.ContainsKey(key);
+        }
+
+        public void Add(Uuid key, ContractQuotes value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public bool Remove(Uuid key)
+        {
+            throw new NotSupportedException();
+        }
+
+        public bool TryGetValue(Uuid key, out ContractQuotes value)
+        {
+            return _contractQuotes.TryGetValue(key, out value);
+        }
+
+        public ContractQuotes this[Uuid key]
+        {
+            get { return _contractQuotes[key]; }
+            set { throw new NotSupportedException(); }
+        }
+
+        public ICollection<Uuid> Keys
+        {
+            get { return _contractQuotes.Keys; }
+        }
+
+        public ICollection<ContractQuotes> Values
+        {
+            get { return _contractQuotes.Values; }
+        }
+    }
+}
---
+++ IronSmarkets/Data/MarketQuotes.cs
@@ -0,0 +1,71 @@
+// Copyright (c) 2011 Smarkets Limited
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use, copy,
+// modify, merge, publish, distribute, sublicense, and/or sell copies
+// of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IronSmarkets.Data
+{
+    public class MarketQuotes
+    {
+        private static readonly IDictionary<Proto.Seto.PriceType, string> PriceTypeStrings =
+            new Dictionary<Proto.Seto.PriceType, string> {
+            { Proto.Seto.PriceType.PRICEPERCENTODDS, "percent-odds" }
+        };
+        private static readonly IDictionary<Proto.Seto.QuantityType, string> QuantityTypeStrings =
+            new Dictionary<Proto.Seto.QuantityType, string> {
+            { Proto.Seto.QuantityType.QUANTITYPAYOFFCURRENCY, "payoff-currency" }
+        };
+
+        private readonly Uuid _uuid;
+        private readonly IContractQuotesMap _contractQuotes;
+        private readonly string _priceType;
+        private readonly string _quantityType;
+
+        public Uuid Uuid { get { return _uuid; } }
+        public IContractQuotesMap ContractQuotes { get { return _contractQuotes; } }
+        public string PriceType { get { return _priceType; } }
+        public string QuantityType { get { return _quantityType; } }
+
+        private MarketQuotes(
+            Uuid uuid,
+            IContractQuotesMap contractQuotes,
+            string priceType,
+            string quantityType)
+        {
+            _uuid = uuid;
+            _contractQuotes = contractQuotes;
+            _priceType = priceType;
+            _quantityType = quantityType;
+        }
+
+        internal static MarketQuotes FromSeto(Proto.Seto.MarketQuotes setoQuotes)
+        {
+            return new MarketQuotes(
+                Uuid.FromUuid128(setoQuotes.Market),
+                ContractQuotesMap.FromSeto(setoQuotes.ContractQuotes),
+                PriceTypeStrings[setoQuotes.PriceType],
+                QuantityTypeStrings[setoQuotes.QuantityType]);
+        }
+    }
+}
